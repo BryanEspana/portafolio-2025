86dc5b57a23a047abc63e773b090b901ac47de65
Bryan España <esp21550@uvg.edu.gt>
2025-10-23 22:57:07 -0600

    Refactors project to use app directory
    Removes legacy `pages` directory structure and related configuration.
    This change moves the project structure to use the `app` directory, aligning with newer Next.js conventions,
    improving organization and utilizing newer features like server components.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
 import { AuthProvider } from '../contexts/AuthContext'
-import '../app/globals.css'
+import '../styles/globals.css'
 
 function App({ Component, pageProps }: AppProps) {
   return (
